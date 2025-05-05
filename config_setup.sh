4a08d44626df2ca8936fbf41a273ba9e74141933
Maksymilian Graczyk <maksymilian.graczyk@cern.ch>
2025-05-05 11:02:00 +0200

    CMake: Fix parent directory error with custom ADAPTYST_CONFIG_PATH

--- config_setup.sh
+++ config_setup.sh
@@ -4,7 +4,7 @@
 # Please follow the Adaptyst installation guide instead. #
 ##########################################################
 if [[ -f $1 ]]; then
-    sed -ie "sperf_path=.*perf_path=$2" $1
+    sed -e "sperf_path=.*perf_path=$2" $1 > $3
 else
-    echo "perf_path=$2" > $1
+    echo "perf_path=$2" > $3
 fi
