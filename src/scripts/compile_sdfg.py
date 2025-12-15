2a3d413c3639db6e52853ab9b92f743336fae92e
Maksymilian Graczyk <maksymilian.graczyk@cern.ch>
2025-12-15 14:49:27 +0100

    Remove -march=native from DaCe flags when running on RISC-V

--- src/scripts/compile_sdfg.py
+++ src/scripts/compile_sdfg.py
@@ -1,10 +1,21 @@
 # SPDX-FileCopyrightText: 2025 CERN
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+from dace import Config
 from dace.sdfg import sdfg
 import json
+import platform
 
 def compile(sdfg_str: str, out_path: str):
+    machine = platform.machine()
+
+    if machine.startswith('rv32') or \
+       machine.startswith('rv64') or \
+       machine.startswith('riscv'):
+        flags = Config.get('compiler', 'cpu', 'args')
+        flags = flags.replace('-march=native', '')
+        Config.set('compiler', 'cpu', 'args', value=flags)
+
     graph = sdfg.SDFG.from_json(json.loads(sdfg_str))
     graph.compile(output_file=out_path,
                   return_program_handle=False)
